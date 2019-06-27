3f313c4d6cc890384f9d4427bf711f8cf27dcd9b
Misat11 <misa.turek1@gmail.com>
2019-06-27 17:13:27 +0200

    Fix water rebuilding

--- plugin/src/main/java/misat11/bw/listener/WorldListener.java
+++ plugin/src/main/java/misat11/bw/listener/WorldListener.java
@@ -1,12 +1,16 @@
 package misat11.bw.listener;
 
 import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBurnEvent;
 import org.bukkit.event.block.BlockFadeEvent;
 import org.bukkit.event.block.BlockFormEvent;
+import org.bukkit.event.block.BlockFromToEvent;
 import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.EntityChangeBlockEvent;
 import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
 import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.world.StructureGrowEvent;
@@ -77,13 +81,12 @@ public class WorldListener implements Listener {
 			}
 		}
 	}
-	
+
 	@EventHandler
 	public void onExplode(EntityExplodeEvent event) {
 		if (event.isCancelled()) {
 			return;
 		}
-
 
 		for (String s : Main.getGameNames()) {
 			Game game = Main.getGame(s);
@@ -93,7 +96,7 @@ public class WorldListener implements Listener {
 				}
 			}
 		}
-		
+
 	}
 
 	@EventHandler
@@ -112,13 +115,13 @@ public class WorldListener implements Listener {
 			}
 		}
 	}
-	
+
 	@EventHandler
 	public void onCreatureSpawn(CreatureSpawnEvent event) {
 		if (event.isCancelled() || event.getSpawnReason() == SpawnReason.CUSTOM) {
 			return;
 		}
-		
+
 		for (String gameName : Main.getGameNames()) {
 			Game game = Main.getGame(gameName);
 			if (game.getStatus() == GameStatus.RUNNING && game.getOriginalOrInheritedPreventSpawningMobs()) {
@@ -128,11 +131,52 @@ public class WorldListener implements Listener {
 				}
 			} else if (game.getStatus() == GameStatus.WAITING) {
 				if (game.getLobbyWorld() == event.getLocation().getWorld()) {
-					if (event.getLocation().distanceSquared(game.getLobbySpawn()) <= Math.pow(Main.getConfigurator().config.getInt("prevent-lobby-spawn-mobs-in-radius"), 2)) {
+					if (event.getLocation().distanceSquared(game.getLobbySpawn()) <= Math
+							.pow(Main.getConfigurator().config.getInt("prevent-lobby-spawn-mobs-in-radius"), 2)) {
 						event.setCancelled(true);
 						return;
 					}
 				}
+			}
+		}
+	}
+
+	@EventHandler
+	public void onLiquidFlow(BlockFromToEvent event) {
+		if (event.isCancelled()) {
+			return;
+		}
+
+		for (String gameName : Main.getGameNames()) {
+			Game game = Main.getGame(gameName);
+			if (GameCreator.isInArea(event.getBlock().getLocation(), game.getPos1(), game.getPos2())) {
+				if (game.getStatus() == GameStatus.RUNNING) {
+					Block block = event.getToBlock();
+					System.out.println(block);
+					if (block.getType() == Material.AIR
+							|| game.getRegion().isBlockAddedDuringGame(block.getLocation())) {
+						game.getRegion().addBuildedDuringGame(block.getLocation());
+					} else {
+						event.setCancelled(true);
+					}
+				} else if (game.getStatus() != GameStatus.DISABLED) {
+					event.setCancelled(true);
+				}
+			}
+		}
+
+	}
+
+	@EventHandler
+	public void onEntityChangeBlock(EntityChangeBlockEvent event) {
+		if (event.isCancelled()) {
+			return;
+		}
+
+		for (String gameName : Main.getGameNames()) {
+			Game game = Main.getGame(gameName);
+			if (game.getStatus() != GameStatus.DISABLED) {
+				event.setCancelled(true);
 			}
 		}
 	}
