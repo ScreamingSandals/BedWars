1556e3f5540e89316adbeeda295fda3fe0645c1d
Misat11 <misa.turek1@gmail.com>
2019-07-15 16:43:52 +0200

    preparing api for special items

---
+++ api/src/main/java/misat11/bw/api/special/ArrowBlocker.java
@@ -0,0 +1,5 @@
+package misat11.bw.api.special;
+
+public interface ArrowBlocker extends SpecialItem {
+
+}
---
+++ api/src/main/java/misat11/bw/api/special/TNTSheep.java
@@ -0,0 +1,7 @@
+package misat11.bw.api.special;
+
+import org.bukkit.entity.LivingEntity;
+
+public interface TNTSheep extends SpecialItem {
+	public LivingEntity getEntity();
+}
