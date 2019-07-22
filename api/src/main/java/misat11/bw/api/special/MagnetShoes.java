52b21cb80e584b02cf7e9d90f2d1d4df5a75fe3a
Misat11 <misa.turek1@gmail.com>
2019-07-22 19:46:02 +0200

    magnet shoes!!!

--- api/src/main/java/misat11/bw/api/special/MagnetShoes.java
+++
@@ -1,9 +0,0 @@
-package misat11.bw.api.special;
-
-import org.bukkit.entity.Player;
-
-public interface MagnetShoes extends SpecialItem {
-	public Player getWearer();
-	
-	public boolean isAnybodyWearing();
-}
--- plugin/src/main/java/misat11/bw/special/listener/MagnetShoesListener.java
+++ plugin/src/main/java/misat11/bw/special/listener/MagnetShoesListener.java
@@ -1,13 +1,20 @@
 package misat11.bw.special.listener;
 
+import java.util.Random;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.inventory.ItemStack;
 
+import misat11.bw.Main;
+import misat11.bw.api.APIUtils;
 import misat11.bw.api.events.BedwarsApplyPropertyToBoughtItem;
 
 public class MagnetShoesListener implements Listener {
+	
+	// Class for special item is not needed in this case (so this special item is not registered in game)
 
 	public static final String MAGNET_SHOES_PREFIX = "Module:MagnetShoes:";
 	
@@ -15,12 +22,35 @@ public class MagnetShoesListener implements Listener {
 	public void onMagnetShoesRegistered(BedwarsApplyPropertyToBoughtItem event) {
 		if (event.getPropertyName().equalsIgnoreCase("magnetshoes")) {
 			ItemStack stack = event.getStack();
-
 			
+			int probability = event.getIntProperty("probability");
 
-			//APIUtils.hashIntoInvisibleString(stack, specialString);
+			APIUtils.hashIntoInvisibleString(stack, MAGNET_SHOES_PREFIX + probability);
+		}
+	}
+	
+	@EventHandler
+	public void onDamage(EntityDamageEvent event) {
+		if (event.isCancelled() || !(event.getEntity() instanceof Player)) {
+			return;
 		}
 		
+		Player player = (Player) event.getEntity();
+		if (Main.isPlayerInGame(player)) {
+			ItemStack boots = player.getInventory().getBoots();
+			if (boots != null) {
+				String magnetShoes = APIUtils.unhashFromInvisibleStringStartsWith(boots, MAGNET_SHOES_PREFIX);
+				if (magnetShoes != null) {
+					int probability = Integer.parseInt(magnetShoes.split(":")[2]);
+					Random r = new Random();
+					int roll = r.nextInt(101);
+					if (roll <= probability) {
+					      event.setCancelled(true);
+					      player.damage(event.getDamage());
+					}
+				}
+			}
+		}
 	}
 
 }
