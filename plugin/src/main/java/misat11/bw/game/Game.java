2a4db421d097e63a93f13300bbd3b24698893b3a
iamceph <granik24@hotmail.cz>
2019-07-28 10:32:48 +0200

    Changed permission for start item to 'bw.vip.startitem'

--- plugin/src/main/java/misat11/bw/game/Game.java
+++ plugin/src/main/java/misat11/bw/game/Game.java
@@ -557,7 +557,7 @@ public class Game implements misat11.bw.api.Game {
 			player.player.getInventory().setItem(leavePosition, leave);
 		}
 
-		if (player.player.hasPermission("bw.vip") || player.player.hasPermission("misat11.bw.vip")) {
+		if (player.player.hasPermission("bw.vip.startitem") || player.player.hasPermission("misat11.bw.vip.startitem")) {
 			int vipPosition = Main.getConfigurator().config.getInt("hotbar.start", 1);
 			if (vipPosition >= 0 && vipPosition <= 8) {
 				ItemStack startGame = Main.getConfigurator().readDefinedItem("startgame", "DIAMOND");
