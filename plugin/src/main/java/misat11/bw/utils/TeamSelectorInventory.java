780ce20407bbc0d846139dd2d00781906d6cf39b
Misat11 <misa.turek1@gmail.com>
2019-08-05 16:15:26 +0200

    team selector is now using sgui, updated to sgui 0.0.8-snap6

--- plugin/src/main/java/misat11/bw/utils/TeamSelectorInventory.java
+++ plugin/src/main/java/misat11/bw/utils/TeamSelectorInventory.java
@@ -4,13 +4,17 @@ import misat11.bw.Main;
 import misat11.bw.api.events.BedwarsOpenTeamSelectionEvent;
 import misat11.bw.game.Game;
 import misat11.bw.game.Team;
+import misat11.lib.sgui.MapReader;
+import misat11.lib.sgui.Options;
+import misat11.lib.sgui.SimpleGuiFormat;
+import misat11.lib.sgui.builder.FormatBuilder;
+import misat11.lib.sgui.events.PostActionEvent;
+
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
@@ -21,62 +25,46 @@ import static misat11.lib.lang.I18n.i18nonly;
 
 public class TeamSelectorInventory implements Listener {
 
-	private Inventory inv;
-	private Main plugin;
 	private Game game;
+	private SimpleGuiFormat format;
 
 	public TeamSelectorInventory(Main plugin, Game game) {
-		this.plugin = plugin;
 		this.game = game;
-		int slotCount = 9;
-		if (game.getTeams().size() > 9) {
-			slotCount = 18;
-		}
-		if (game.getTeams().size() > 18) {
-			slotCount = 54; // What??? There are more than 18 teams?
-		}
-
-		String teamSelectionName = i18nonly("team_selection_name", "Select team - %arena%").replace("%arena%", game.getName());
-		inv = Bukkit.getServer().createInventory(null, slotCount, teamSelectionName);
-
+		
+		Options options = new Options();
+		options.setPrefix(i18nonly("team_selection_name", "Select team - %arena%").replace("%arena%", game.getName()));
+		options.setShowPageNumber(false);
+		options.setRender_header_start(54); // Disable header
+		options.setRender_offset(0);
+		int teamCount = game.getTeams().size();
+		if (teamCount <= 9) {
+			options.setRender_actual_rows(1);
+		} else if (teamCount <= 18) {
+			options.setRender_actual_rows(2);
+		} 
+		
+		format = new SimpleGuiFormat(options);
+		
+		FormatBuilder builder = new FormatBuilder();
+		
 		for (Team team : game.getTeams()) {
 			ItemStack teamStack = team.color.getWool();
 			ItemMeta im = teamStack.getItemMeta();
 			im.setDisplayName(team.color.chatColor + team.name);
 			im.setLore(Arrays.asList(i18n("click_to_join_team", false)));
 			teamStack.setItemMeta(im);
-			inv.addItem(teamStack);
+			builder.add(teamStack).set("team", team);
 		}
+		
+		format.load(builder);
+		
+		format.generateData();
 
-		Bukkit.getServer().getPluginManager().registerEvents(this, this.plugin);
+		Bukkit.getServer().getPluginManager().registerEvents(this, plugin);
 	}
 
 	public void destroy() {
 		HandlerList.unregisterAll(this);
-		inv.clear();
-	}
-
-	@EventHandler
-	public void onInventoryClick(InventoryClickEvent e) {
-		if (!e.getInventory().equals(inv))
-			return;
-		e.setCancelled(true);
-		if (!(e.getWhoClicked() instanceof Player)) {
-			e.getWhoClicked().closeInventory();
-			return; // How this happened?
-		}
-		Player player = (Player) e.getWhoClicked();
-		if (!Main.isPlayerInGame(player)) {
-			player.closeInventory();
-			return;
-		}
-		if (e.getCurrentItem() == null) {
-			return;
-		}
-		if (e.getCurrentItem().getItemMeta() == null) {
-			return;
-		}
-		game.selectTeam(Main.getPlayerGameProfile(player), e.getCurrentItem().getItemMeta().getDisplayName());
 	}
 
 	public void openForPlayer(Player player) {
@@ -87,7 +75,22 @@ public class TeamSelectorInventory implements Listener {
 			return;
 		}
 		
-		player.openInventory(inv);
+		format.openForPlayer(player);
+	}
+	
+	@EventHandler
+	public void onPostAction(PostActionEvent event) {
+		if (event.getFormat() != format) {
+			return;
+		}
+		
+		MapReader reader = event.getItem().getReader();
+		if (reader.containsKey("team")) {
+			Team team = (Team) reader.get("team");
+			System.out.println(team);
+			game.selectTeam(Main.getPlayerGameProfile(event.getPlayer()), team.getName());
+			event.getPlayer().closeInventory();
+		}
 	}
 
 }
