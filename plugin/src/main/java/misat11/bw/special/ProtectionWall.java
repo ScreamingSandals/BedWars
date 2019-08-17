23aa6aa9d501bc4fc2c7a41ba6331611ab78bbf2
iamceph <granik24@hotmail.cz>
2019-08-17 17:00:10 +0200

    Better coloring methods, fixed coloring in shop

--- plugin/src/main/java/misat11/bw/special/ProtectionWall.java
+++ plugin/src/main/java/misat11/bw/special/ProtectionWall.java
@@ -110,7 +110,7 @@ public class ProtectionWall extends SpecialItem implements misat11.bw.api.specia
 
 	private void addBlockToList(Block block)  {
 		wallBlocks.add(block);
-		game.getRegion().addBuildedDuringGame(block.getLocation());
+		game.getRegion().addBuiltDuringGame(block.getLocation());
 	}
 
 	private void removeBlockFromList(Block block) {
@@ -180,21 +180,19 @@ public class ProtectionWall extends SpecialItem implements misat11.bw.api.specia
 					continue;
 				}
 
+				ItemStack coloredStack = Main.applyColor(
+						TeamColor.fromApiColor(team.getColor()), new ItemStack(buildingMaterial));
 				if (Main.isLegacy()) {
-					ItemStack itemStack = new ItemStack(buildingMaterial);
-					itemStack = ColorChanger.changeLegacyStackColor(itemStack, TeamColor.fromApiColor(team.getColor()));
-
-					placedBlock.setType(itemStack.getType());
+					placedBlock.setType(coloredStack.getType());
 					try {
 						// The method is no longer in API, but in legacy versions exists
-						Block.class.getMethod("setData", byte.class).invoke(placedBlock, (byte) itemStack.getDurability());
+						Block.class.getMethod("setData", byte.class).invoke(placedBlock, (byte) coloredStack.getDurability());
 					} catch (Exception e) {
 						e.printStackTrace();
 					}
 				} else {
-					placedBlock.setType(ColorChanger.changeMaterialColor(buildingMaterial, TeamColor.fromApiColor(team.getColor())));
+					placedBlock.setType(coloredStack.getType());
 				}
-
 				addBlockToList(placedBlock);
 			}
 		}
