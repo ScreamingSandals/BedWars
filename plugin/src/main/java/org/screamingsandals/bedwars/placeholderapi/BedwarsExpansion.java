9dc74575e65dd61271cad037ce033a4500106faa
Ceph <granik24@hotmail.cz>
2019-11-14 18:20:24 +0100

    Fixed bad return of "current_team_colored"

--- plugin/src/main/java/org/screamingsandals/bedwars/placeholderapi/BedwarsExpansion.java
+++ plugin/src/main/java/org/screamingsandals/bedwars/placeholderapi/BedwarsExpansion.java
@@ -116,7 +116,7 @@ public class BedwarsExpansion extends PlaceholderExpansion {
                     } else {
                         CurrentTeam team = game.getPlayerTeam(gPlayer);
                         if (team != null) {
-                            return team.getColor() + team.getName();
+                            return ChatColor.getByChar(team.getColor().name()) + team.getName();
                         } else {
                             return ChatColor.RED + "none";
                         }
