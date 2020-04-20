05036c338a45f3359590dcd343103d61c9a815db
notfoundname <48317169+notfoundname@users.noreply.github.com>
2020-04-20 18:57:43 +0800

    Update ProtectionWall.java

--- plugin/src/main/java/org/screamingsandals/bedwars/special/ProtectionWall.java
+++ plugin/src/main/java/org/screamingsandals/bedwars/special/ProtectionWall.java
@@ -196,15 +196,21 @@ public class ProtectionWall extends SpecialItem implements org.screamingsandals.
 
             MiscUtils.sendActionBarMessage(player, i18nonly("specials_protection_wall_created").replace("%time%", Integer.toString(breakingTime)));
 
-            item.setAmount(item.getAmount() - 1);
-            player.getInventory().setItem(player.getInventory().getHeldItemSlot(), item); // legacy fix
+            if(item.getAmount() > 1) {
+        		item.setAmount(item.getAmount() - 1);
+        	} else {
+        		player.getInventory().remove(item);
+            }
             player.updateInventory();
         } else {
             game.registerSpecialItem(this);
 
             MiscUtils.sendActionBarMessage(player, i18nonly("specials_protection_wall_created_unbreakable"));
-            item.setAmount(item.getAmount() - 1);
-            player.getInventory().setItem(player.getInventory().getHeldItemSlot(), item); // legacy fix
+            if(item.getAmount() > 1) {
+        		item.setAmount(item.getAmount() - 1);
+        	} else {
+        		player.getInventory().remove(item);
+        	}
             player.updateInventory();
         }
     }
