118438e4079f3d10f796563f13e767853c3cd77b
Misat11 <misa.turek1@gmail.com>
2020-06-29 11:17:59 +0200

    Welcome PersistentDataContainer!!

--- api/src/main/java/org/screamingsandals/bedwars/api/APIUtils.java
+++ api/src/main/java/org/screamingsandals/bedwars/api/APIUtils.java
@@ -1,28 +1,49 @@
 package org.screamingsandals.bedwars.api;
 
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
+import java.util.Map;
 
+import com.google.gson.Gson;
 import org.bukkit.ChatColor;
+import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
+import org.bukkit.plugin.Plugin;
 
 /**
  * @author Bedwars Team
  *
  */
 public class APIUtils {
+	public static final String BEDWARS_NAMESPACED_KEY = "screaming-bedwars-hidden-api";
+
 	/**
 	 * @param stack
 	 * @param hash
 	 */
 	public static void hashIntoInvisibleString(ItemStack stack, String hash) {
 		ItemMeta meta = stack.getItemMeta();
-		List<String> lore = meta.hasLore() ? meta.getLore() : new ArrayList<>();
+		try {
+			NamespacedKey key = new NamespacedKey((Plugin) BedwarsAPI.getInstance(), BEDWARS_NAMESPACED_KEY);
+			PersistentDataContainer container = meta.getPersistentDataContainer();
+			List<String> propertyLines = new ArrayList<>();
+			if (container.has(key, PersistentDataType.STRING)) {
+				String oldString = container.get(key, PersistentDataType.STRING);
+				propertyLines.addAll((List<String>) new Gson().fromJson(oldString, List.class));
+			}
+			propertyLines.add(hash);
+			container.set(key, PersistentDataType.STRING, new Gson().toJson(propertyLines));
+		} catch (Throwable ignored) {
+			// Use the Lore API instead
+			List<String> lore = meta.hasLore() ? meta.getLore() : new ArrayList<>();
 
-		lore.add(convertToInvisibleString(hash));
-		meta.setLore(lore);
-
+			lore.add(convertToInvisibleString(hash));
+			meta.setLore(lore);
+		}
 		stack.setItemMeta(meta);
 	}
 
@@ -34,17 +55,30 @@ public class APIUtils {
 	public static String unhashFromInvisibleStringStartsWith(ItemStack stack, String startsWith) {
 		ItemMeta meta = stack.getItemMeta();
 		try {
-			if (meta.hasLore()) {
-				List<String> lore = meta.getLore();
-				for (String s : lore) {
-					String unhidden = returnFromInvisibleString(s);
+			NamespacedKey key = new NamespacedKey((Plugin) BedwarsAPI.getInstance(), BEDWARS_NAMESPACED_KEY);
+			PersistentDataContainer container = meta.getPersistentDataContainer();
+			if (container.has(key, PersistentDataType.STRING)) {
+				String oldString = container.get(key, PersistentDataType.STRING);
+				List<String> propertyLines = (List<String>) new Gson().fromJson(oldString, List.class);
+				for (String unhidden : propertyLines) {
 					if (unhidden.startsWith(startsWith)) {
 						return unhidden;
 					}
 				}
 			}
-		} catch (NullPointerException ignored) {
-			return null;
+		} catch (Throwable ignored) {
+			try {
+				if (meta.hasLore()) {
+					List<String> lore = meta.getLore();
+					for (String s : lore) {
+						String unhidden = returnFromInvisibleString(s);
+						if (unhidden.startsWith(startsWith)) {
+							return unhidden;
+						}
+					}
+				}
+			} catch (NullPointerException ignored2) {
+			}
 		}
 		return null;
 	}
@@ -57,17 +91,31 @@ public class APIUtils {
 	public static boolean unhashFromInvisibleString(ItemStack stack, String hash) {
 		ItemMeta meta = stack.getItemMeta();
 		try {
-			if (meta.hasLore()) {
-				List<String> lore = meta.getLore();
-				for (String s : lore) {
-					String unhidden = returnFromInvisibleString(s);
+			NamespacedKey key = new NamespacedKey((Plugin) BedwarsAPI.getInstance(), BEDWARS_NAMESPACED_KEY);
+			PersistentDataContainer container = meta.getPersistentDataContainer();
+			if (container.has(key, PersistentDataType.STRING)) {
+				String oldString = container.get(key, PersistentDataType.STRING);
+				List<String> propertyLines = (List<String>) new Gson().fromJson(oldString, List.class);
+				for (String unhidden : propertyLines) {
 					if (unhidden.equals(hash)) {
 						return true;
 					}
 				}
 			}
-		} catch (NullPointerException ignored) {
-			return false;
+		} catch (Throwable ignored) {
+			try {
+				if (meta.hasLore()) {
+					List<String> lore = meta.getLore();
+					for (String s : lore) {
+						String unhidden = returnFromInvisibleString(s);
+						if (unhidden.equals(hash)) {
+							return true;
+						}
+					}
+				}
+			} catch (NullPointerException ignored2) {
+				return false;
+			}
 		}
 		return false;
 	}
