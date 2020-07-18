f69b46639199f04bd70deaa044edb2ae18066032
notfoundname <48317169+notfoundname@users.noreply.github.com>
2020-07-18 22:21:08 +0800

    now

--- api/src/main/java/org/screamingsandals/bedwars/api/events/BedwarsGameChangedStatusEvent.java
+++ api/src/main/java/org/screamingsandals/bedwars/api/events/BedwarsGameChangedStatusEvent.java
@@ -8,30 +8,30 @@ import org.screamingsandals.bedwars.api.game.Game;
  * @author Bedwars Team
  */
 public class BedwarsGameChangedStatusEvent extends Event {
-  private static final HandlerList handlers = new HandlerList();
-  private Game game;
+    private static final HandlerList handlers = new HandlerList();
+    private Game game;
     
-  /**
-   * @param game
-   */
-  public BedwarsGameChangedStatusEvent (Game game) {
-    this.game = game;
-  }
+    /**
+     * @param game
+     */
+    public BedwarsGameChangedStatusEvent (Game game) {
+        this.game = game;
+    }
     
-  @Override
-  public HandlerList getHandlers() {
-    return BedwarsGameChangedStatusEvent.handlers;
-  }
+    @Override
+    public HandlerList getHandlers() {
+        return BedwarsGameChangedStatusEvent.handlers;
+    }
 	
-  /**
-   * @return game
-   */
-  public Game getGame() {
-    return this.game;
-  }
+    /**
+     * @return game
+     */
+    public Game getGame() {
+        return this.game;
+    }
 
-  public static HandlerList getHandlerList() {
-    return BedwarsGameChangedStatusEvent.handlers;
-  }
+    public static HandlerList getHandlerList() {
+        return BedwarsGameChangedStatusEvent.handlers;
+    }
 
 }
