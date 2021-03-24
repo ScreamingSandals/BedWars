7d65515bbbf1143a19df12c7671744a9ba8f05a7
Misat11 <misa.turek1@gmail.com>
2021-03-24 09:50:47 +0100

    custom language files work again

--- plugin/src/main/java/org/screamingsandals/bedwars/lang/BedWarsLangService.java
+++ plugin/src/main/java/org/screamingsandals/bedwars/lang/BedWarsLangService.java
@@ -7,19 +7,24 @@ import org.apache.commons.lang.LocaleUtils;
 import org.screamingsandals.bedwars.config.MainConfig;
 import org.screamingsandals.lib.lang.Lang;
 import org.screamingsandals.lib.lang.LangService;
-import org.screamingsandals.lib.lang.container.TranslationContainer;
 import org.screamingsandals.lib.utils.AdventureHelper;
 import org.screamingsandals.lib.utils.annotations.Service;
 import org.screamingsandals.lib.utils.annotations.methods.OnEnable;
+import org.screamingsandals.lib.utils.annotations.parameters.DataFolder;
 import org.screamingsandals.lib.utils.logger.LoggerWrapper;
+import org.spongepowered.configurate.BasicConfigurationNode;
 import org.spongepowered.configurate.ConfigurateException;
 import org.spongepowered.configurate.gson.GsonConfigurationLoader;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 @Service(dependsOn = {
@@ -27,8 +32,12 @@ import java.util.stream.Collectors;
 })
 @RequiredArgsConstructor
 public class BedWarsLangService extends LangService {
+    public static final Pattern LANGUAGE_PATTERN = Pattern.compile("[a-z]{2}-[A-Z]{2}");
+
     private final MainConfig mainConfig;
     private final LoggerWrapper logger;
+    @DataFolder("languages")
+    private final Path languagesFolder;
 
     {
         Lang.initDefault(this);
@@ -37,6 +46,7 @@ public class BedWarsLangService extends LangService {
     @SneakyThrows
     @OnEnable
     public void onEnable() {
+        // TODO: Multi language + Language updater
         Locale locale;
         try {
             locale = LocaleUtils.toLocale(mainConfig.node("locale").getString("en_US").replace("-", "_"));
@@ -53,6 +63,7 @@ public class BedWarsLangService extends LangService {
 
         if (internalLanguageDefinition == null) {
             logger.error("Can't load default Language Definition for Screaming BedWars!");
+            return;
         }
 
         var languages = internalLanguageDefinition
@@ -77,26 +88,26 @@ public class BedWarsLangService extends LangService {
                 .findFirst()
                 .map(entry -> {
                     try {
-                        return TranslationContainer.of(
+                        return LayeredTranslationContainer.of(
                                 GsonConfigurationLoader
                                         .builder()
                                         .source(() -> new BufferedReader(new InputStreamReader(BedWarsLangService.class.getResourceAsStream("/" + entry.getValue()))))
                                         .build()
-                                        .load(),
-                                null);
+                                        .load()
+                        );
                     } catch (ConfigurateException e) {
                         logger.error("Can't load base language file en_US!", e);
                         e.printStackTrace();
                         return null;
                     }
                 })
-                .orElse(null);
+                .orElseGet(() -> LayeredTranslationContainer.of(BasicConfigurationNode.root()));
 
-        if (us == null) {
+        if (us.isEmpty()) {
             logger.warn("Language definitions don't contain en_US file!");
         }
 
-        if (!locale.equals(Locale.US)) {
+        if (!finalLocale.equals(Locale.US)) {
             fallbackContainer = languages
                     .stream()
                     .filter(entry -> entry.getKey().equals(finalLocale))
@@ -108,13 +119,16 @@ public class BedWarsLangService extends LangService {
                     )
                     .map(entry -> {
                         try {
-                            return TranslationContainer.of(
+                            return LayeredTranslationContainer.of(
+                                    us,
                                     GsonConfigurationLoader
                                             .builder()
                                             .source(() -> new BufferedReader(new InputStreamReader(BedWarsLangService.class.getResourceAsStream("/" + entry.getValue()))))
                                             .build()
                                             .load(),
-                                    us);
+                                    BasicConfigurationNode.root(),
+                                    BasicConfigurationNode.root()
+                            );
                         } catch (ConfigurateException e) {
                             logger.error("Can't load language file!", e);
                             e.printStackTrace();
@@ -126,7 +140,44 @@ public class BedWarsLangService extends LangService {
             fallbackContainer = us;
         }
 
-        // TODO updater and custom translations
+        if (Files.exists(languagesFolder)) {
+            try (var stream = Files.walk(languagesFolder.toAbsolutePath())) {
+                stream.filter(Files::isRegularFile)
+                        .forEach(file -> {
+                            var name = file.getFileName().toString();
+                            if (Files.exists(file) && Files.isRegularFile(file) && name.toLowerCase().endsWith(".json")) {
+                                var matcher = LANGUAGE_PATTERN.matcher(name);
+                                if (matcher.find()) {
+                                    try {
+                                        var locale1 = LocaleUtils.toLocale(matcher.group().replace("-", "_"));
 
+                                        if (finalLocale.equals(locale1)) {
+                                            ((LayeredTranslationContainer) fallbackContainer)
+                                                    .setCustomNode(GsonConfigurationLoader
+                                                            .builder()
+                                                            .path(file)
+                                                            .build()
+                                                            .load()
+                                                    );
+                                        } else if (Locale.US.equals(locale1)) {
+                                            us.setCustomNode(GsonConfigurationLoader
+                                                    .builder()
+                                                    .path(file)
+                                                    .build()
+                                                    .load()
+                                            );
+                                        }
+                                    } catch (IllegalArgumentException | ConfigurateException ex) {
+                                        logger.warn("Invalid language file in languages directory: " + name, ex);
+                                    }
+                                }
+                            }
+                        });
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Files.createDirectory(languagesFolder);
+        }
     }
 }
