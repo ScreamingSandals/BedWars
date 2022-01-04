8023d8f5ef53f11ba089e72ff4f9b0ce1d2eb9c4
Misat11 <misa.turek1@gmail.com>
2022-01-04 21:26:10 +0100

    [ci skip] license headers

--- api/src/main/java/org/screamingsandals/bedwars/api/variants/Variant.java
+++ api/src/main/java/org/screamingsandals/bedwars/api/variants/Variant.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2022 ScreamingSandals
+ *
+ * This file is part of Screaming BedWars.
+ *
+ * Screaming BedWars is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Screaming BedWars is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Screaming BedWars. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package org.screamingsandals.bedwars.api.variants;
 
 import org.screamingsandals.bedwars.api.config.ConfigurationContainer;
