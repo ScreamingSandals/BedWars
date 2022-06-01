147d6c55998e95dc42e3f701ca9bb235932e9860
copokbl <zaneharrison2006@icloud.com>
2022-06-02 09:07:59 +1000

    Removed unused imports

--- plugin/src/main/java/org/screamingsandals/bedwars/commands/JoinCommand.java
+++ plugin/src/main/java/org/screamingsandals/bedwars/commands/JoinCommand.java
@@ -21,22 +21,16 @@ package org.screamingsandals.bedwars.commands;
 
 import cloud.commandframework.Command;
 import cloud.commandframework.CommandManager;
-import org.screamingsandals.bedwars.api.game.GameStatus;
 import org.screamingsandals.bedwars.econ.EconomyProvider;
-import org.screamingsandals.bedwars.game.GameImpl;
 import org.screamingsandals.bedwars.game.GameManagerImpl;
 import org.screamingsandals.bedwars.lang.LangKeys;
 import org.screamingsandals.bedwars.player.PlayerManagerImpl;
-import org.screamingsandals.bedwars.utils.MiscUtils;
 import org.screamingsandals.lib.lang.Message;
 import org.screamingsandals.lib.player.PlayerWrapper;
 import org.screamingsandals.lib.sender.CommandSenderWrapper;
 import org.screamingsandals.lib.utils.annotations.Service;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
-import java.util.Random;
 
 @Service(dependsOn = {
         EconomyProvider.class
