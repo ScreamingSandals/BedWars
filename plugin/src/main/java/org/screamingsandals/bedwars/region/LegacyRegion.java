65df0904752bbb284b5de630911888d11d2680d4
Misat11 <20199703+Misat11@users.noreply.github.com>
2024-07-17 17:54:25 +0200

    fix: #542

--- plugin/src/main/java/org/screamingsandals/bedwars/region/LegacyRegion.java
+++ plugin/src/main/java/org/screamingsandals/bedwars/region/LegacyRegion.java
@@ -148,7 +148,7 @@ public class LegacyRegion implements Region {
             BlockState feedState = blockFeed.getState();
 
             headState.setType(brokenBlockTypes.get(blockHead));
-            feedState.setType(brokenBlockTypes.get(blockFeed));
+            feedState.setType(brokenBlockTypes.getOrDefault(blockFeed, brokenBlockTypes.get(blockHead)));
             headState.setRawData((byte) 0x0);
             feedState.setRawData((byte) 0x8);
             feedState.update(true, false);
